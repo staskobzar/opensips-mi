cd30b0c5768f125cea304c09c2afc967bd36a672
Stas Kobzar <stas@modulis.ca>
2013-04-17 12:17:42 -0400

    Simplecov config

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 group :test do
   gem 'coveralls', require: false
+  gem 'simplecov' #, :require => false
   gem 'mocha'
 end
 
--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,17 @@
+require 'simplecov'
+require 'coveralls'
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start 
+
 require 'minitest/autorun'
 require 'mocha/setup'
 require 'stringio'
 require 'opensips/mi'
-
-require 'coveralls'
-Coveralls.wear!
 
 class MiniTest::Unit::TestCase
 
